45074bbfc6e495d469cc663f03e4952065ed790b
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2023-02-23 17:23:11 +0500

    setup custom user

--- flextest/organizations/models.py
+++ flextest/organizations/models.py
@@ -1,22 +1,32 @@
 from django.db import models
 from django.core.validators import RegexValidator
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
 
 # Create your models here.
 class Organization(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
     
+
     
-class user(models.Model):
+class User(AbstractUser):
+    object = CustomUserManager()
     phone_regex = RegexValidator(regex=r'^\+?1?\d{11}$',
                                  message="Не корректный формат номера телефона")
     
-    email = models.EmailField(max_length=254)
-    password = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
-    firstname = models.CharField(max_length=50)
+    username = None    
+    email = models.EmailField(
+            verbose_name='email address',
+            max_length=255,
+            unique=True,
+    )
     phone = models.CharField(validators=[phone_regex], max_length=12, blank=True)
     icon = models.ImageField(upload_to="uploads/users/")
     organizations = models.ManyToManyField(Organization)
     
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
