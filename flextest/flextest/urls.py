1f195064850bda44c0adba893ef941ecb1778a1d
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2023-02-24 17:06:15 +0500

    bugs fixes

--- flextest/flextest/urls.py
+++ flextest/flextest/urls.py
@@ -21,7 +21,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('organizations.urls')),
-    path('api-token-auth', views.obtain_auth_token),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
